7b636bb6d5a86d40f85369cfd2067171da05bbcb
laubreyual <laubreyual@gmail.com>
2020-04-09 01:50:17 +0800

    Finished web layout for homepage

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
+
+function changeIcon() {
+	var navButton = document.getElementsByClassName('navbar-toggler')[0];
+	var navTrigger = document.getElementById('trigger');
+	if(navButton.classList.contains('collapsed')){
+		navTrigger.classList.remove('fa-bars');
+		navTrigger.classList.add('fa-times');
+	}else {
+		navTrigger.classList.remove('fa-times');
+		navTrigger.classList.add('fa-bars');
+	}
+}
